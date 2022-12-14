88e3cb0481f98722e5de8a2fef67074c1855c544
Adiv Eliyahu <Adiveliyahu3@gmail.com>
2022-12-14 14:56:08 +0200

    unittest for Academic Staff Member

--- adivtest/app/tests.py
+++ adivtest/app/tests.py
@@ -20,3 +20,21 @@ class StudentTestCase(TestCase):
         self.assertEqual(student_data['role'], 1, "User should be role 1 (student)")
         
 auth.current_user = None        
+
+
+class AcademicStaffTestCase(TestCase): 
+    def setUp(self):
+        auth.sign_in_with_email_and_password("ASMtest@test.com", "111111") 
+        
+    def test_ASM_login(self): 
+        email, passw = "ASMtest@test.com", "111111"
+        student = auth.sign_in_with_email_and_password(email, passw)
+        student_data = db.child('users').child(email[:email.index('@')]).get().val()
+
+        #Check if the user is really looged in
+        self.assertTrue(auth.current_user)
+        #Checking if user data is equal to db data
+        self.assertEqual(student['idToken'], auth.current_user['idToken'], "It is the same user!")
+        self.assertEqual(student_data['role'], 3, "User should be role 3 (student)")
+        
+auth.current_user = None
