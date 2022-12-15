739b95bd2f853402ccb13c53bd8934a96aa571e6
MysteriousFool <yovelnir@gmail.com>
2022-12-15 18:35:12 +0200

    added remove_user function

--- adivtest/adivtest/urls.py
+++ adivtest/adivtest/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('main_Student/', views.main_Student, name = 'main_Student'),       #path for Student user 
     path('main_ASM/',views.main_ASM,name='main_ASM'),         #path to main page for ASM   
     path('create_user/',views.create_user, name='create_user'),
+    path('delete_user/',views.remove_user, name='delete_user'),
     path('logout/', views.logout, name='logout')
 ]
